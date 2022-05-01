c5695866a49a55d52a1cbcf0c1de0bc86c36c54f
Niall <niallos11@gmail.com>
2022-05-01 19:44:33 +0000

    add renderScores arrow function to be used to return score & highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,8 @@ const boxs = document.querySelectorAll('.box')
 const target = document.querySelector('.target')
 const timeLeft = document.querySelector('.time-left')
 
+renderScores = () => {
 document.getElementById('js-score').innerText = score;
 document.getElementById('js-highscore').innerText = highscore;
+}
+renderScores();
