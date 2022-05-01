58ad8215c71870260a06ab6121f63f83cf651634
Niall <niallos11@gmail.com>
2022-05-01 17:22:21 +0000

    add getElementById to get vaule of ID html objects js-score & js-highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 const boxs = document.querySelectorAll('.box')
 const target = document.querySelector('.target')
 const timeLeft = document.querySelector('.time-left')
+
+document.getElementById('js-score').innerText = score;
+document.getElementById('js-highscore').innerText = highscore;
