90d379c03ebf6d821356e38047e9ff77b701af6a
Niall <niallos11@gmail.com>
2022-06-15 00:30:36 +0000

    add semi-colons to fix jshint errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,32 @@ let highscore = localStorage.getItem('highscore') || score;
 let whacPosition;
 
 //const variables 
-const boxs = document.querySelectorAll('.box')
-const target = document.querySelector('.target')
-const timeLeft = document.querySelector('.time-left')
+const boxs = document.querySelectorAll('.box');
+const target = document.querySelector('.target');
+const timeLeft = document.querySelector('.time-left');
 
 //Render Score
-renderScores = () => {
+let renderScores = () => {
 document.getElementById('js-score').innerText = score;
 document.getElementById('js-highscore').innerText = highscore;
-}
+};
 
 // if logic to update and render score + highscore
-updateHighscore = (score) => {
+let updateHighscore = (score) => {
   if (score > highscore) {
     highscore = score;
   }
-}
+};
 // Random Box function
 function randomBox() {
   boxs.forEach(box => {
-    box.classList.remove('target')
-  })
+    box.classList.remove('target');
+  });
 // Random location of target in boxes 
-  let randomBox = boxs[Math.floor(Math.random() * 9)]
-  randomBox.classList.add('target')
+  let randomBox = boxs[Math.floor(Math.random() * 9)];
+  randomBox.classList.add('target');
 
-  whacPosition = randomBox.classList  // Declare hit position = to the random box hit
+  whacPosition = randomBox.classList;  // Declare hit position = to the random box hit
 }
 // Event Listener when clicked on box with target to add result with whacPosition
 boxs.forEach(box => {
@@ -39,42 +39,42 @@ boxs.forEach(box => {
       updateHighscore(score);
       renderScores();
       localStorage.setItem('highscore',score); // save highscore to local storage
-      whacPosition = null
+      whacPosition = null;
     }
-  })
-})
+  });
+});
 // function for speed to move Target 
 function moveTarget() {
-    timerId = setInterval(randomBox, 600)
+    timerId = setInterval(randomBox, 600);
   }
 // timer countdown 
-let timerId
+let timerId;
 
-let currentTime
+let currentTime;
 
-let countDownTimerId
+let countDownTimerId;
 
 // timer function 
 
 function countDown() {
-    currentTime--
-    timeLeft.textContent = currentTime
+    currentTime--;
+    timeLeft.textContent = currentTime;
     if (currentTime == 0) {
-      clearInterval(countDownTimerId)
-      clearInterval(timerId)
-      alert(' Game Over, You scored ' + score)
-      alert(' High Score score to beat is ' + highscore)
-      whacPosition = null
+      clearInterval(countDownTimerId);
+      clearInterval(timerId);
+      alert(' Game Over, You scored ' + score);
+      alert(' High Score score to beat is ' + highscore);
+      whacPosition = null;
 }
 // function to play game 
 }
 function jsButton(){
-  clearInterval(countDownTimerId) 
-  clearInterval(timerId)
-  currentTime = 30
+  clearInterval(countDownTimerId); 
+  clearInterval(timerId);
+  currentTime = 30;
   moveTarget();
-  countDownTimerId = setInterval(countDown, 1000)
-  score = 0
+  countDownTimerId = setInterval(countDown, 1000);
+  score = 0;
   clearInterval(score);
   renderScores();
 } 
